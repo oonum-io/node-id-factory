e006da5f9f3ab13e26f48608b7e6d21d3d2fa2ba
Jeff Dimond <jeff@oonum.com>
2020-03-27 12:22:12 -0600

    package updates, .npmignore updated, circleci yaml updated, code structure brought up to standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier', 'import', 'json'],
   settings: {
@@ -16,6 +11,21 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/indent': [2, 2],
-    '@typescript-eslint/quotes': ['error', 'single']
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/no-var-requires': 1,
+    '@typescript-eslint/ban-ts-ignore': 0,
   },
+  env: {
+    'node': true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:json/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript"
+  ]
 };
