b9992f1e04256f55ed94c8695455ee1807edaf23
Nate Bohman <natrinicle@gmail.com>
2020-09-10 11:28:48 -0600

    Add mongoObjectId Generator
    Set the default export to use Mongo's ObjectId converted to a string but
    keep the old string generation just in case it becomes necessary in the
    future.

--- __tests__/new-id.test.ts
+++ __tests__/new-id.test.ts
@@ -1,18 +1,37 @@
-import newId from '../src';
+import newId, { mongoObjectId, uuid58Id } from '../src';
 
-describe('function newId()', () => {
+describe('function default newId()', () => {
   test('contrived: generates unique ids', () => {
     const list: string[] = [];
-    let i = -1;
     const count = 100;
-    while (++i < count) {
+    for (let i = 0; i < count; i++) {
       list.push(newId());
     }
-    i = -1;
-    while (++i < count) {
-      const id = list.pop();
-      console.log(id);
-      expect(list.indexOf(id)).toBe(-1);
+    const uniqueIds = Array.from(new Set(list));
+    expect(list.length).toBe(uniqueIds.length);
+  });
+});
+
+describe('function mongoObjectId()', () => {
+  test('mongoObjectId: generates unique ids', () => {
+    const list: string[] = [];
+    const count = 100;
+    for (let i = 0; i < count; i++) {
+      list.push(mongoObjectId());
     }
+    const uniqueIds = Array.from(new Set(list));
+    expect(list.length).toBe(uniqueIds.length);
+  });
+});
+
+describe('function uuid58Id()', () => {
+  test('uuid58Id: generates unique ids', () => {
+    const list: string[] = [];
+    const count = 100;
+    for (let i = 0; i < count; i++) {
+      list.push(uuid58Id());
+    }
+    const uniqueIds = Array.from(new Set(list));
+    expect(list.length).toBe(uniqueIds.length);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import { Types } from 'mongoose';
 import { v4 } from 'uuid';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const base = require('base-x');
 
 const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 const base58 = base(ALPHABET);
 
 export default (): string => {
+  return mongoObjectId();
+};
+
+export function mongoObjectId(): string {
+  return Types.ObjectId().toString();
+}
+
+export function uuid58Id(): string {
   const buf = Buffer.alloc(16);
   const uuid = v4(null, buf);
   return base58.encode(uuid);
-};
+}
